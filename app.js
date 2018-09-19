64d47d1ca5522ae53b263431608c111f49f6b616
donjoe <giumontanaro@gmail.com>
2018-09-19 14:11:45 +0200

    add src attribute to video when the dom load

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 import data from './data.js';
+const videos = data.categories[0].videos;
+const videoTag = document.getElementById("video")
+
+//First load of document
+document.addEventListener("DOMContentLoaded", function() {
+  const src = videos[0].sources.join();
+  videoTag.src = src;
+});
